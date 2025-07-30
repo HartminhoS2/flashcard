e1c4665d3d9ca1f57d7948ac45a2d9cef26fadc5
HartminhoS2 <pedro.hartmann.silva@escola.pr.gov.br>
2025-07-30 08:54:36 -0300

    Add files via upload

---
+++ flashcard-main/Flashcard/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+'Programação',
+'Oque é Python?',
+'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+'Geografia',
+'Qual a capital a França?',
+'A capital da França é Paris'
+)
+
+ criaCartao(
+'Programação',
+'Oque é uma funcão?',
+'Uma função é um bloco de código que executa alguma tarefa'
+)
